37e5f5a8ba0382857a0a025ae12738835049ae23
tahap4rlar <tahaparlarr@gmail.com>
2025-12-16 14:18:00 +0300

    dashboard

---
+++ MedicalHub/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using MedicalHub.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalHub.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class DashboardController(ApplicationDbContext db) : Controller
+{
+    public IActionResult Index()
+    {
+        ViewBag.SliderCount = db.Sliders.Where(p => p.IsActive).Count();
+
+        return View();
+    }
+}
